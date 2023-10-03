b2f0b7bb47ca8fa27ea75a370e375be24a34eddd
JPrime88 <145236626+JPrime88@users.noreply.github.com>
2023-10-03 17:10:58 -0400

    #32 only upload index.html if a content version has a change (#33)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -21,6 +21,7 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
     key = "error.html"
   }
 }
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
@@ -28,6 +29,10 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath
 
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -60,4 +65,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
